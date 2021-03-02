e7ffecb85ed0d16b8e0f57d5b25eddbd3ef1868f
Michał Szyba <m.szyba@gmail.com>
2021-03-02 12:57:54 +0100

    added first part renderTask

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// const apikey = '';
 const apihost = 'https://todo-api.coderslab.pl';
+const apikey = "";
 
 function apiListAllTasks() {
     return fetch(
@@ -15,11 +15,6 @@ function apiListAllTasks() {
         );
 }
 
-apiListAllTasks()
-    .then(response => {
-        console.log("Odpowiedx" + response);
-    });
-
 function apiCreateTask(title, description) {
     return fetch(
         apihost + '/api/tasks',
@@ -28,63 +23,70 @@ function apiCreateTask(title, description) {
                 'Authorization': apikey,
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({title: title, description: description})
+            body: JSON.stringify({ title: title, description: description })
         }
     ).then(resp => {
-        if(!resp.ok) {
-            alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
-        }
+        if(!resp.ok) { alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny'); }
         return resp.json();
     });
 }
 
-apiCreateTask("test tytuł", "test opis")
-    .then(resp => {
-        console.log("Odpowiedź serwera: " + resp);
+function apiListTasks() {
+    return fetch(
+        apihost + '/api/tasks',
+        { headers: { 'Authorization': apikey }}
+        ).then(resp => {
+            if(!resp.ok) { alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny'); }
+            return resp.json();
     });
+}
 
+function renderTask(taskId, title, description, status) {
+    const section = document.createElement("section");
+    section.className = "card mt-5 shadow-sm";
+    document.querySelector("main").appendChild(section);
 
+    const headerDiv = document.createElement("div");
+    headerDiv.className = "card-header d-flex justify-content-between align-items-center";
+    section.appendChild(headerDiv);
 
-//
-// const form = document.querySelector("form");
-// const addTaskButton = form.querySelector("button");
-// form.querySelector("input").name
-//
-// console.log(addTaskButton);
-//
-// addTaskButton.addEventListener("click", evt => {
-//     evt.preventDefault();
-//
-//     const title = form.elements["title"].value
-//     const description = form.elements["description"].value
-//
-//     const section = document.createElement("section");
-//     section.className = "card mt-5 shadow-sm";
-//     document.querySelector("main").appendChild(section);
-//
-//     const headerDiv = document.createElement("div");
-//     headerDiv.className = "card-header d-flex justify-content-between align-items-center";
-//     section.appendChild(headerDiv);
-//
-//     const headerLeftDiv = document.createElement('div');
-//     headerDiv.appendChild(headerLeftDiv);
-//
-//     const h5 = document.createElement('h5');
-//     h5.innerText = title;
-//     headerLeftDiv.appendChild(h5);
-//
-//     const h6 = document.createElement('h6');
-//     h6.className = 'card-subtitle text-muted';
-//     h6.innerText = description;
-//     headerLeftDiv.appendChild(h6);
-//
-//     const headerRightDiv = document.createElement('div');
-//     headerDiv.appendChild(headerRightDiv);
-// });
-//
-//
-//
-//
-// document.addEventListener('DOMContentLoaded', function() {
-//
-// });
+    const headerLeftDiv = document.createElement("div");
+    headerDiv.appendChild(headerLeftDiv);
+
+    const h5 = document.createElement("h5");
+    h5.innerText = title;
+    headerLeftDiv.appendChild(h5);
+
+    const h6 = document.createElement("h6");
+    h6.className = "card-subtitle text-muted";
+    h6.innerText = description;
+    headerLeftDiv.appendChild(h6);
+
+    const headerRightDiv = document.createElement("div");
+    headerDiv.appendChild(headerRightDiv);
+
+    if (status === "open") {
+        const finishButton = document.createElement("button");
+        finishButton.className = "btn btn-dark btn-sm js-task-open-only";
+        finishButton.innerText = "Finish";
+        headerRightDiv.appendChild(finishButton);
+    }
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "btn btn-outline-danger btn-sm ml-2";
+    deleteButton.innerText = "Delete";
+    headerRightDiv.appendChild(deleteButton);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    apiListTasks()
+        .then(resp => {
+                resp.data.forEach( task => {
+                    function (task) {
+                        renderTask(task.id, task.title, task.description, task.status);
+                    }
+                );
+            }
+        );
+});
+
