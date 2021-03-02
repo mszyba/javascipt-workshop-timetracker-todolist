c4955fad3afbe4f7c5a8c1295895a1d7af257f26
Michał Szyba <m.szyba@gmail.com>
2021-03-02 11:58:58 +0100

    function apiListAllTasks() added

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const apikey = '';
+// const apikey = '';
 const apihost = 'https://todo-api.coderslab.pl';
 
+function apiListAllTasks() {
+    return fetch(
+        apihost + '/api/tasks',
+        {headers: {'Authorization': apikey}}
+    )
+        .then(resp => {
+            if (!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        }
+    );
+}
 
-
-document.addEventListener('DOMContentLoaded', function() {
-
-});
+apiListAllTasks()
+    .then(response => {
+        console.log("Odpowiedx" + response);
+    });
